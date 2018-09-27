582e52ffd2f6c8625320c1d06717de8becf15a43
Alan Luiz <alanlviana@hotmail.com>
2018-09-26 22:18:35 -0300

    ThreadStaticAttribute

---
+++ 1-005-UsandoThreadStaticAttribute/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_005_UsandoThreadStaticAttribute
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _campo;
+
+        static void Main(string[] args)
+        {
+            // Thread A
+            new Thread(() =>
+            {
+                for(int i = 0; i< 10; i++)
+                {
+                    _campo++;
+                    Console.WriteLine("Thread A: {0}", _campo);
+                }
+            }).Start();
+
+            // Thread B
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _campo++;
+                    Console.WriteLine("Thread B: {0}", _campo);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
