85f93153edf8c4b7b53ba45cecdc09cfdca51d35
Alan Luiz <alanlviana@hotmail.com>
2018-09-26 22:08:51 -0300

    Primeiro Projeto

--- 1-001-CriandoThreads/Program.cs
+++ 1-001-CriandoThreads/Program.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _1_001_CriandoThreads
 {
     class Program
     {
+        public static void ThreadMethod() {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Fazendo algum trabalho.");
+                Thread.Sleep(0);
+            }
+
+            // Aguarda Thread terminar.
+            t.Join();
+
+
         }
     }
 }
