b25b1a6d2f5e1247b6d1654dcbe9805661152824
Alan Luiz <alanlviana@hotmail.com>
2018-09-27 22:43:54 -0300

    Novos projetos com a classe Task

---
+++ 1-013-UsandoTaskFactory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_013_UsandoTaskFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(() =>
+            {
+                var valores = new int[3];
+
+                var ft = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+
+                ft.StartNew(() => valores[0] = 10);
+                ft.StartNew(() => valores[1] = 20);
+                ft.StartNew(() => valores[2] = 30);
+
+                return valores;
+
+            });
+
+            var finalTask = task.ContinueWith((t) =>
+            {
+                foreach (int i in t.Result)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+
+            finalTask.Wait();
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 1-014-UsandoTaskWaitAll/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_014_UsandoTaskWaitAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 10;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 20;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 30;
+            });
+
+            Task.WaitAll(tasks);
+
+        }
+    }
+}
